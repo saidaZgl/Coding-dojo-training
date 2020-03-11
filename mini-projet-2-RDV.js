367878f7af5166a921daa64e093c124d34bb9ad1
Saida Zghal <zghal.saida@orange.fr>
2020-03-11 21:08:15 +0100

    solutions alternatives au mini projet 2 RDV

--- mini-projet-2-RDV.js
+++ mini-projet-2-RDV.js
@@ -93,3 +93,91 @@ if (finRdvMinutes < 10) {
 } else {
   console.log("Le RDV se terminera à " + finRdvHeures + "h" + finRdvMinutes);
 }
+
+/* SOLUTION ALTERNATIVE 1
+utiliser le nombre total de minutes
+pour comparer les horaires
+*/
+
+// Début du RDV : 15h40
+let debutRdvHeures = 15;
+let debutRdvMinutes = 40;
+
+// Fin de journée : 16h30
+let finJourneeHeures = 16;
+let finJourneeMinutes = 30;
+
+// Durée du RDV : 25 minutes
+let duree = 25;
+
+// ETAPE 1 : Calcul de la fin du RDV
+let finRdvHeures = debutRdvHeures;
+let finRdvMinutes = debutRdvMinutes + duree;
+
+// ETAPE 1.1 : Gestion du cas particulier où les minutes sont >= 60
+if (finRdvMinutes >= 60) {
+  finRdvHeures = finRdvHeures + 1;
+  finRdvMinutes = finRdvMinutes - 60;
+}
+
+// ETAPE 1.2 Calcul des minutes totales pour comparaison
+let finRdvTotalMinutes = finRdvHeures * 60 + finRdvMinutes;
+let finJourneeTotalMinutes = finJourneeHeures * 60 + finJourneeMinutes;
+
+// ETAPE 2 : Est-ce que la fin du RDV est avant la fin de la journée ?
+if (finRdvTotalMinutes < finJourneeTotalMinutes) {
+  console.log("RDV OK");
+} else {
+  console.log("RDV impossible");
+}
+
+// ETAPE 3 : Affichage de l'heure de fin du RDV au format HHhMM
+if (finRdvMinutes < 10) {
+  console.log("Le RDV se terminera à " + finRdvHeures + "h0" + finRdvMinutes);
+} else {
+  console.log("Le RDV se terminera à " + finRdvHeures + "h" + finRdvMinutes);
+}
+
+/* SOLUTION ALTERNATIVE 2 
+calculer la durée maximale possible d'un RDV et la
+comparer directement avec la durée
+*/
+
+// Début du RDV : 15h40
+let debutRdvHeures = 15;
+let debutRdvMinutes = 40;
+
+// Fin de journée : 16h30
+let finJourneeHeures = 16;
+let finJourneeMinutes = 30;
+
+// Durée du RDV : 25 minutes
+let duree = 25;
+
+// ETAPE 1 : Calcul de la fin du RDV
+let finRdvHeures = debutRdvHeures;
+let finRdvMinutes = debutRdvMinutes + duree;
+
+// ETAPE 1.1 : Gestion du cas particulier où les minutes sont >= 60
+if (finRdvMinutes >= 60) {
+  finRdvHeures = finRdvHeures + 1;
+  finRdvMinutes = finRdvMinutes - 60;
+}
+
+// ETAPE 2 : Est-ce que la fin du RDV est avant la fin de la journée ?
+let dureeRdvMax =
+  (finJourneeHeures - debutRdvHeures) * 60 +
+  (finJourneeMinutes - debutRdvMinutes);
+
+if (duree < dureeRdvMax) {
+  console.log("RDV OK");
+} else {
+  console.log("RDV impossible");
+}
+
+// ETAPE 3 : Affichage de l'heure de fin du RDV au format HHhMM
+if (finRdvMinutes < 10) {
+  console.log("Le RDV se terminera à " + finRdvHeures + "h0" + finRdvMinutes);
+} else {
+  console.log("Le RDV se terminera à " + finRdvHeures + "h" + finRdvMinutes);
+}
